e23b6fe0a10d9aff5eb93f1ddb7feead90d70892
BehroozMirmolavi-GLA <39798678+BehroozMirmolavi-GLA@users.noreply.github.com>
2020-02-17 14:33:32 +0000

    Day 2 complete

--- adventofcode2019.py
+++ adventofcode2019.py
@@ -1,50 +1,71 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 import pandas
 import math
 
-
-# In[19]:
-
-
+#Day 1 part 1
 data = pandas.read_clipboard(header=None)
-
-
-# In[20]:
-
-
-print(data)
-
-
-# In[23]:
-
 
 data.columns = ['V1']
 
+def fuel(data):   
+   fuel = data.V1.apply(lambda x: math.floor(x/3)-2).sum()
+   print(fuel)
+   return;
+fuel(data)
 
-# In[35]:
+#Day 1 part 2
+
+def fuelforfuel(data):
+    y = 0
+    x = data
+    while x > 0:
+        x = max(math.floor(x / 3) - 2, 0)
+        if x <= 0:
+            break
+        y = x + y
+    return y
+
+data.V1.apply(fuelforfuel).sum()
+
+#Day 2 part 1
+data = pandas.read_clipboard(header=None,sep = ',', squeeze = True).values.tolist()[0]
+
+def int_code_program(program_input,noun,verb):
+    program = copy.deepcopy(program_input)
+    program[1] = noun
+    program[2] = verb
+    pc = 0
+    while pc < len(program):
+        opcode = program[pc]
+        opcode1 = program[program[pc + 1]]
+        opcode2 = program[program[pc + 2]]
+        destination = program[pc+3]
+        if opcode == 1:
+            program[destination] = opcode1 + opcode2
+            pc += 4
+        elif opcode == 2:
+            program[destination] = opcode1 * opcode2
+            pc += 4
+        elif opcode == 99:
+            break
+        else:
+            print('unknown opcode')
+            break
+    return program[0]
 
 
-data.V2 = data.V1.apply(lambda x: math.floor(x/3)-2)
+int_code_program(data,12,2)
 
-
-# In[36]:
-
-
-print(data)
-
-
-# In[37]:
-
-
-data.V2.sum()
-
-
-# In[ ]:
+#Day 2 part 2
+#find a noun verb combination that outputs 19690720
+i = 0
+possibilities = [(x, y) for x in range(99) for y in range(99)]
+while int_code_program(data,n,v) != 19690720:
+        i += 1
+        n = possibilities[i][0]
+        v = possibilities[i][1]
 
 
 
